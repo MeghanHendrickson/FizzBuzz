3dd0cca12dc13cc8e0ac526183a39ccce7276550
mghnhndrcksn <meghan.hendrickson@mail.utoronto.ca>
2024-05-16 19:40:58 -0400

    "refactored to extract helper method"

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Call the main method of FizzBuzz
+        // pass args as the parameter to the relative equivalent method
+        FizzBuzz.main(args);
+    }
+}
